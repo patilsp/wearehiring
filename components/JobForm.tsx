c595e8eba013dd89c9ab6cdf715bf5460d84376a
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-10-29 12:36:28 +0530

    ui changes

--- components/JobForm.tsx
+++ components/JobForm.tsx
@@ -2,9 +2,8 @@
 "use client"
 
 import React from 'react';
-import Link from 'next/link';
-
-import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+import { Button } from "@/registry/new-york/ui/button"
 import {
   Card,
   CardContent,
--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com','images.unsplash.com'],
   },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    }
-    return config
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
